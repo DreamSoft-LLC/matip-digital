2dfd8a0f3a86cdf287145fee61e4a5f92deec57f
unknown <penielnyinaku@gmail.com>
2021-09-10 23:44:59 +0800

    cgan

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
 // export const API_SERVER = "https://morning-coast-29203.herokuapp.com";
-export const API_SERVER = "https://6476-102-176-94-163.ngrok.io";
+export const API_SERVER = " https://c201-154-160-19-191.ngrok.io";
 
 export const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
